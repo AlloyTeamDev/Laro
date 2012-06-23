0a80432d1d596b3cfc6c485afab6305756e92391
hongru <hongru.chenhr@gmail.com>
2012-06-23 10:50:22 +0800

    update animation events

--- src/actions/animation.js
+++ src/actions/animation.js
@@ -37,17 +37,24 @@ Laro.register('.action', function (La) {
                 "pivoty": 128,
                 "framerate": 30,
                 "pivotx": 256,
-                "events": []
+                "events": [
+                    {
+                        frame: 1,
+                        name: 'attack',
+                        time: ..
+                    }
+                ]
             }
      * @param {Array} frames: 从json配置文件里面获得的每帧的位置信息
      * @return 帧动画信息实例
      */
 
     var Animation = Class(function (anim, frames) {
+        if (anim.framerate == undefined) anim.framerate = 20;
         extend(this, anim);
 
         this.frames = frames;
-        if (anim.framerate == undefined) anim.framerate = 20;
+        
         // 这个动画执行需要的时间
         this.animationLength = frames.length / anim.framerate;
     }).methods({
@@ -56,7 +63,10 @@ Laro.register('.action', function (La) {
      */ 
 
         /**
-         * 获取动画时间内指定时间段[from, to]中插入的事件
+         * 获取动画时间内指定时间段[from, to]中插入的事件, 通常是前一帧和后一帧之间的时间间隔
+         * 事件获取可以通过 指定frame 和 time时间点 两种方式指定触发时机。 优先以 frame 指定的来计算，方便配置
+         * 如果以frame 来配置的话，事件触发时机为 指定frame 开始的时间点。
+         *
          * @param {Number} from: 起始时间
          * @param {Number} to: 结束时间
          * @return {Array} 指定时间区间内事件列表
@@ -65,6 +75,9 @@ Laro.register('.action', function (La) {
             var events = [];
             for (var e = 0; e < this.events.length; e ++) {
                 var evt = this.events[e];
+                if (evt.frame != undefined) {
+                    evt.time = Math.max(0, parseInt(evt.frame) - 1)/this.framerate;
+                }
                 if (evt.time >= from && evt.time < to) {
                     events.push(evt.name);
                 }
@@ -81,6 +94,9 @@ Laro.register('.action', function (La) {
             var first = -1;
             for (var e = 0; e < this.events.length; e ++) {
                 var evt = this.events[e];
+                if (evt.frame != undefined) {
+                    evt.time = Math.max(0, parseInt(evt.frame) - 1)/this.framerate;
+                }
                 if (evt.time > from && evt.time < to) {
                     if (first != -1) return first;
                     first = evt.time;
@@ -97,6 +113,9 @@ Laro.register('.action', function (La) {
                 evt;
             for (e = 0; e < this.events.length; e++) {
                 evt = this.events[e];
+                if (evt.frame != undefined) {
+                    evt.time = Math.max(0, parseInt(evt.frame) - 1)/this.framerate;
+                }
                 if (evt.time >= from && evt.time < end) {
                     events.push(evt.name);
                 }
@@ -104,6 +123,9 @@ Laro.register('.action', function (La) {
 
             for (e = 0; e < this.events.length; e ++) {
                 evt = this.events[e];
+                if (evt.frame != undefined) {
+                    evt.time = Math.max(0, parseInt(evt.frame) - 1)/this.framerate;
+                }
                 if (evt.time >= start && evt.time < to) {
                     events.push(evt.name);
                 }
